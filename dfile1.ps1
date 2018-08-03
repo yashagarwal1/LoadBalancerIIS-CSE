08dd9e4b66db9987dcd9a1e8312e92423f294dd4
yashagarwal1 <40016488+yashagarwal1@users.noreply.github.com>
2018-08-03 11:34:53 +0530

    Update dfile1.ps1

--- dfile1.ps1
+++ dfile1.ps1
@@ -3,7 +3,7 @@
 Install-WindowsFeature web-server -IncludeManagementTools
 start-sleep 15
 $dest = "c:\inetpub\wwwroot\iisstart.htm"
-$defaultHtmPath = "https://strgcse.blob.core.windows.net/cseblo/vm1.htm"
+$defaultHtmPath = "https://raw.githubusercontent.com/yashagarwal1/LoadBalancerIIS-CSE/master/vm1.htm"
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("$defaultHtmPath","C:\ew.htm")
 get-content "C:\ew.htm" | Out-File "C:\inetpub\wwwroot\iisstart.htm"
