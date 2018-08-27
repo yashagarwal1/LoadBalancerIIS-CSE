5c7a9f611a9089e4f96e46b1f2c284e2c83c7c0b
yashagarwal1 <40016488+yashagarwal1@users.noreply.github.com>
2018-08-27 16:32:36 +0530

    Create cse4.ps1

---
+++ cse4.ps1
@@ -0,0 +1,9 @@
+<# Cusatom Script for Windows #>
+
+Install-WindowsFeature web-server -IncludeManagementTools
+start-sleep 15
+$dest = "c:\inetpub\wwwroot\iisstart.htm"
+$defaultHtmPath = "https://raw.githubusercontent.com/yashagarwal1/LoadBalancerIIS-CSE/master/vm4.htm"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("$defaultHtmPath","C:\ew.htm")
+get-content "C:\ew.htm" | Out-File "C:\inetpub\wwwroot\iisstart.htm"
